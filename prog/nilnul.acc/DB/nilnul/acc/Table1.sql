81cbf0ef06919eaab30cc1a8a5a61836748f28be
wangyoutian <wangyoutian@nilnul.com>
2019-10-16 06:16:48 +0800

    commit workspace index for possibly restoring work site later

---
+++ prog/nilnul.acc/DB/nilnul/acc/Table1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [nilnul].[Acc]
+(
+	[id] bigINT NOT NULL PRIMARY KEY identity
+	,
+	[name] nvarchar(4000)
+	,
+	pass_salt nvarchar(400) --salt used to hash pass
+	,
+	[pass] nvarchar(4000) --must be hashed
+)
---
+++ prog/svr/db_/nilnul/bak/Script1.sql
@@ -0,0 +1,3 @@
+﻿backup database nilnul4tutorial
+	to 
+		disk=N'D:\170203\data\nilnul._sqlSvr_\_KEN_(Git\tutorial_\_nilnulAcc_\sample(Git\data\191011.bak'  -- the file must be no-existant such as to be created anew
