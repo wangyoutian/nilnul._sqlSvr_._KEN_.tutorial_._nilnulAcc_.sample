ceb03d496682d8dfc4f9b0f29f641fc1407e7e85
WANG,Youtian <wangyoutian@nilnul.com>
2022-11-06 00:37:40 +0800

    db vs fs: bak, detach, offline;

---
+++ prog/_db._SVR_/db/fs/detach/attach.sql
@@ -0,0 +1,9 @@
+﻿CREATE DATABASE MyAdventureWorks   
+    ON (
+		FILENAME = 'C:\MySQLServer\AdventureWorks_Data.mdf'
+	)
+	,   
+    (
+		FILENAME = 'C:\MySQLServer\AdventureWorks_Log.ldf'
+	)   
+    FOR ATTACH;
